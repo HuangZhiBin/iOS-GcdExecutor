657101d716c5bbab2f65837209e800e32d75c72b
黄智彬 <ikrboy@163.com>
2018-12-29 12:14:19 +0800

    no message

---
+++ GcdDownload/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GcdDownload
+//
+//  Created by bin on 2018/12/29.
+//  Copyright © 2018年 BinHuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-GcdDownload/Pods-GcdDownload-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GcdDownload : NSObject
+@end
+@implementation PodsDummy_Pods_GcdDownload
+@end
